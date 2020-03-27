994f962a1807612c5c9021b988dcd5f7c6c8737f
Kuro091 <lcminhhp@gmail.com>
2020-03-27 09:52:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/snapchat/Screens/SignUp/SignUp_Birthday.java
@@ -0,0 +1,16 @@
+package com.example.snapchat.Screens.SignUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.snapchat.R;
+
+public class SignUp_Birthday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up__birthday);
+    }
+}
